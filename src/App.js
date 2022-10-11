969cacfc7e5a7989df4359ef76db5093b2374d83
Linkitsoft <Linkitsoftdeveloper@gmail.com>
2022-10-11 17:11:18 +0500

    fixing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,80 @@
 import { useEffect, useRef, useState } from 'react';
-import './App.css';
+import OTPInput, { ResendOTP } from "otp-input-react";  
+import './App.scss';
 
 function App() {
   const [stateUpdate , setStateUpdate] = useState()
   const [text, setText] = useState([])
+  const [OTP, setOTP] = useState("");
     
   const ref1 = useRef()
   const ref2 = useRef()
   const ref3 = useRef()
   const ref4 = useRef()
 
+
   const a = (myRef, e) => {
     setStateUpdate(!stateUpdate)
     setText([...text, e.target.value])
-    myRef?.current.focus()
-    var one = text[0] = ref1.current.value;
-    var two = text[1] = ref2.current.value;
-    var three = text[2] = ref3.current.value;
-    var four = text[3] = ref4.current.value;
 
+    var one = text[0] = ref1?.current?.value;
+    var two = text[1] = ref2?.current?.value;
+    var three = text[2] = ref3?.current?.value;
+    var four = text[3] = ref4?.current?.value;
+  
+  
     var ans = one + two + three + four;
     ans.toString();
-    console.log("ans ====>", ans)
-    if (four === "") {
-      ref3.current.focus()
-    } else {
-      console.log("four is full")
-    }
 
+    if (myRef === 1) {
+      ref2?.current.focus()
+    } else if (myRef === 2) {
+      ref3.current.focus()
+    } else if (myRef === 3) {
+      ref4.current.focus()
+    } 
+    console.log("ans ====>", ans)
   }
 
 
 
+  const back = (e , res) => {
+   
+    if (e.key === "Backspace") {
+      ref4.current.value = "" 
+      if (res === 4) { 
+        ref4.current.value = "" 
+        ref3.current.focus()
+      } else if (res === 3) {
+          ref4.current.value = "" 
+        ref2.current.focus()
+        // ref2.current.value = ""
+       }else if (res === 2) {
+        ref1.current.focus()
+        // ref1.current.value = ""
+       }
+    }
+
+  }
   
   
 
   return (
     <div className="App">
-      <input maxLength={1} onChange={(e) => a(ref2,e)} ref={ref1} className="box"></input>
-      <input maxLength={1} onChange={(e) => a(ref3,e)} ref={ref2}  className="box"></input>
-      <input maxLength={1} onChange={(e) => a(ref4,e)} ref={ref3} className="box"></input>
-      <input maxLength={1} onChange={(e) => a(ref4, e)} ref={ref4} className="box"></input>
-      {/* <input onChange={() => a(1)} ref={ref1} className="box"></input>
-      <input onChange={() => a(2)} ref={ref2}  className="box"></input>
-      <input onChange={() => a(3)} ref={ref3} className="box"></input>
-      <input onChange={() => a(4)} ref={ref4} className="box"></input> */}
-  
-       <button onClick={() => a()}>Update</button>
+      <OTPInput
+      value={OTP}
+      onChange={setOTP}
+      autoFocus
+      OTPLength={4}
+      otpType="number"
+      disabled={false}
+      secure
+    />
+    <ResendOTP handelResendClick={() => console.log("Resend clicked")} />
+      {/* <input maxLength={1} onKeyDown={(e) => back(e , 1)} onChange={(e) => a(1,e)} ref={ref1} className="box"></input>
+      <input maxLength={1} onKeyDown={(e) => back(e , 2)} onChange={(e) => a(2,e)} ref={ref2}  className="box"></input>
+      <input maxLength={1} onKeyDown={(e) => back(e , 3)} onChange={(e) => a(3,e)} ref={ref3} className="box"></input>
+      <input maxLength={1} onKeyDown={(e) => back(e , 4)} onChange={(e) => a(4, e)} ref={ref4} className="box"></input>   */}
     </div>
   );
 }
